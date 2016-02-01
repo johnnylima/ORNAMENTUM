14bc634b72a65ffb2d314789555e60578b7c8035
johnnylima <rickbatera2012@gmail.com>
2016-02-01 15:10:07 -0200

    Diretiva Toogle Menu
    Todas operações de Toogle do Menu está ok!

--- _app/script/directives/onmtToogleMenuDirective.js
+++ _app/script/directives/onmtToogleMenuDirective.js
@@ -9,20 +9,63 @@
     return {
       controller: function($scope, $element, $attrs) {
         var toogleMenuFilhos = [];
+        var uLink;
+        var vLink = false;
 
+        //registro dos scope's dos links detro de <onmt-toogle-menu-pai>
         this.registrosMenuFilhos = function(menuFilho) {
           toogleMenuFilhos.push(menuFilho);
         };
-        this.closeAll = function() {
-          toogleMenuFilhos.forEach(function(menuFilho) {
-            menuFilho.isOpened = false;
-          })
+
+        //controle dos <onmt-toogle-menu-filho> que serão fechados/aberto
+        this.closeAll = function(sLink) {
+
+          if (vLink == false) {
+            //Primeira vez
+            // console.log('blok 1');
+            toogleMenuFilhos.forEach(function(menuFilho) {
+              if (menuFilho.link == sLink.link) {
+                //console.log(sLink.link);
+                uLink = sLink.link;
+                menuFilho.isOpened = true;
+              } else {
+                menuFilho.isOpened = false;
+              }
+            })
+            vLink = true;
+
+          } else if (sLink.link == uLink) {
+            //A partir da segunda vez
+            // console.log('blok 2');
+            // console.log(sLink.link);
+
+            toogleMenuFilhos.forEach(function(menuFilho) {
+              menuFilho.isOpened = false;
+              vLink = false;
+            })
+
+
+          } else {
+            // console.log(sLink.link + ' : ' + uLink);
+            // console.log('blok 3');
+
+            toogleMenuFilhos.forEach(function(menuFilho) {
+              if (menuFilho.link == sLink.link) {
+                //console.log(sLink.link);
+                menuFilho.isOpened = true;
+                uLink = sLink.link;
+              } else {
+                menuFilho.isOpened = false;
+              }
+            })
+            vLink = true;
+          }
         }
       }
     };
   });
   // diretiva filha de ontmToogleMenuPai
-  angular.module("ornamentumApp").directive("onmtToogleMenuFilhos", function() {
+  angular.module("ornamentumApp").directive("onmtToogleMenuFilho", function() {
     return {
       templateUrl: "view/toogleMenu.html",
       scope: {
@@ -32,14 +75,13 @@
       },
       require: "^onmtToogleMenuPai",
       link: function(scope, element, attrs, ctrl) {
+        scope.isOpened = false;
         scope.filhos = scope.$eval(attrs.filhos);
         ctrl.registrosMenuFilhos(scope);
-        var registroFilho;
 
         scope.open = function() {
-          ctrl.closeAll();
-
-          scope.isOpened = true;
+          ctrl.closeAll(scope);
+          //scope.isOpened = true;
         }
       }
     };
