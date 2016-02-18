0f9cf1056a6abfc6bc92cca551a5f8b539b012bd
johnnylima <rickbatera2012@gmail.com>
2016-02-18 16:12:09 -0200

    onmtFormWizard

--- _app/script/directives/onmtFormWizardDirective.js
+++ _app/script/directives/onmtFormWizardDirective.js
@@ -1,11 +1,38 @@
 /*
-* Projetado por Johnny Lima
-*/
+ * Projetado por Johnny Lima
+ */
 (function() {
   'use strict';
 
   angular.module("ornamentumApp")
-  .directive("onmtSidenavMenu", function() {
-    
-  });
-});
+    .directive("onmtFormWizard", function() {
+      return {
+        restrict: 'E',
+        replace: true,
+        transclude: true,
+        templateUrl: "view/onmtFormWizardView.html",
+        scope: {
+          // atribute - model: "=",
+          // atribute: "@"
+        },
+        link: function(scope, element, attrs, ctrl) {
+          console.log("ok");
+          /*
+          scope.isOpened = false;
+          scope.filhos = scope.$eval(attrs.filhos);
+          ctrl.registrosMenuFilhos(scope);
+
+          element.bind("mousemove",function(){
+            //console.log(icone);
+          });
+
+          scope.open = function() {
+            ctrl.closeAll(scope);
+            //scope.isOpened = true;
+          }
+          */
+        }
+      };
+
+    });
+})();
