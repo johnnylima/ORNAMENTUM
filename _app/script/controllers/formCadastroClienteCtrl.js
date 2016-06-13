533f1794c3399135a5ce716f99ee615648e5a9f8
johnnylima <rickbatera2012@gmail.com>
2016-06-13 16:27:34 -0300

    Controller Formulário de cadastro de Cliente
    Adicionado as funções de Dialog.

--- _app/script/controllers/formCadastroClienteCtrl.js
+++ _app/script/controllers/formCadastroClienteCtrl.js
@@ -5,7 +5,84 @@
   'use strict';
 
   angular.module("ornamentumApp")
-    .controller("formCadastroClienteCtrl", function($scope) {
-      $scope.tb = [];
-    });
+    .controller("formCadastroClienteCtrl", formDialogClientCtrl);
+
+
+
+
+
+  function formDialogClientCtrl($scope, $mdDialog, $mdMedia) {
+    var alert;
+    $scope.showAlert = showAlert;
+    $scope.showDialog = showDialog;
+    $scope.showAddEnderecoDialog = showAddEnderecoDialog;
+    $scope.showAddContatoDialog = showAddContatoDialog;
+    $scope.items = [1, 2, 3, 4, 5];
+    // Internal method
+    function showAlert() {
+      alert = $mdDialog.alert({
+        title: 'Attention',
+        textContent: 'This is an example of how easy dialogs can be!',
+        ok: 'Close'
+      });
+      $mdDialog
+        .show(alert)
+        .finally(function() {
+          alert = undefined;
+        });
+    }
+
+    function showDialog($event) {
+      var parentEl = angular.element(document.body);
+      $mdDialog.show({
+        parent: parentEl,
+        targetEvent: $event,
+        templateUrl: 'view/cliente/dialog.tmpl.endereco.html',
+        locals: {
+          items: $scope.items
+        },
+        controller: DialogController
+      });
+
+      function DialogController($scope, $mdDialog, items) {
+        $scope.items = items;
+        $scope.closeDialog = function() {
+          $mdDialog.hide();
+        }
+      }
+    }
+
+    function showAddEnderecoDialog(ev) {
+      $mdDialog.show({
+          controller: DialogController,
+          templateUrl: 'view/cliente/endereco.dialog.tmpl.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutsideToClose: true,
+          locals: {
+            items: $scope.items
+          }
+        })
+    }
+
+    function showAddContatoDialog(ev) {
+      $mdDialog.show({
+          controller: DialogController,
+          templateUrl: 'view/cliente/contato.dialog.tmpl.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutsideToClose: true,
+          locals: {
+            items: $scope.items
+          }
+        })
+    }
+
+    function DialogController($scope, $mdDialog, items) {
+      $scope.items = items;
+      $scope.closeDialog = function() {
+        $mdDialog.hide();
+      }
+    }
+  }
 })();
