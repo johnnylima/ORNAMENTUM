68e4c7d06d3a346ef438dfa02af0b3609b6358bc
johnnylima <rickbatera2012@gmail.com>
2016-06-16 14:45:34 -0300

    Controllers
    Implementação: EnderecoDialogCtrl e ContatoDialogCtrl.

--- _app/script/controllers/formCadastroClienteCtrl.js
+++ _app/script/controllers/formCadastroClienteCtrl.js
@@ -5,24 +5,22 @@
   'use strict';
 
   angular.module("ornamentumApp")
-    .controller("formCadastroClienteCtrl", formDialogClientCtrl);
+    .controller("formCadastroClienteCtrl", formCadastroCliente);
 
-  function formDadosClienteCtrl($scope) {
-    $scope.so = "hello";
-    console.log(so);
-  }
-
-
-
-
-
-  function formDialogClientCtrl($scope, $mdDialog, $mdMedia) {
+  function formCadastroCliente($scope, $mdDialog, $mdMedia) {
     var alert;
+
+    /* ------------------------------------ DADOS ------------------------------------ */
+    $scope.dado=[];
+    $scope.dadoContato=[];
+    $scope.dadoEndereco=[];
+
+    /* ------------------------------------ DIALOGS ------------------------------------ */
     $scope.showAlert = showAlert;
     $scope.showDialog = showDialog;
     $scope.showAddEnderecoDialog = showAddEnderecoDialog;
     $scope.showAddContatoDialog = showAddContatoDialog;
-    $scope.items = [1, 2, 3, 4, 5];
+    $scope.items = [];
     // Internal method
     function showAlert() {
       alert = $mdDialog.alert({
@@ -59,34 +57,76 @@
 
     function showAddEnderecoDialog(ev) {
       $mdDialog.show({
-        controller: DialogController,
+        controller: EnderecoDialogCtrl,
         templateUrl: 'view/cliente/endereco.dialog.tmpl.html',
         parent: angular.element(document.body),
         targetEvent: ev,
         clickOutsideToClose: true,
+        scope: $scope,
+        preserveScope: true,
         locals: {
           items: $scope.items
         }
-      })
+      });
+
+      function EnderecoDialogCtrl($scope, $mdDialog, items) {
+        $scope.items = items;
+        $scope.closeDialog = function() {
+          $scope.limparForm();
+          $mdDialog.hide();
+        };
+        $scope.limparForm = function(){
+          delete $scope.endereco;
+          $scope.clienteEnderecoForm.$setPristine();
+        };
+        $scope.AddEndereco = function(endereco){
+          $scope.dadoEndereco.push(angular.copy(endereco));
+          $scope.closeDialog();
+        };
+      }
     }
 
     function showAddContatoDialog(ev) {
       $mdDialog.show({
-        controller: DialogController,
+        controller: ContatoDialogCtrl,
         templateUrl: 'view/cliente/contato.dialog.tmpl.html',
         parent: angular.element(document.body),
         targetEvent: ev,
         clickOutsideToClose: true,
+        scope: $scope,
+        preserveScope: true,
         locals: {
           items: $scope.items
         }
-      })
-    }
+      });
 
-    function DialogController($scope, $mdDialog, items) {
-      $scope.items = items;
-      $scope.closeDialog = function() {
-        $mdDialog.hide();
+      function ContatoDialogCtrl  ($scope, $mdDialog, items) {
+        $scope.items = items;
+        $scope.telefones=[];
+        $scope.closeDialog = function() {
+          $scope.limparForm();
+          $mdDialog.hide();
+        };
+        $scope.limparForm = function(){
+          delete $scope.contato;
+          $scope.clienteContatoForm.$setPristine();
+        };
+        $scope.AddContato = function(contato){
+          $scope.dadoContato.push(angular.copy(contato));
+          $scope.closeDialog();
+        };
+        $scope.removerTelefones = function(){
+          $scope.telefones.pop();
+        };
+        $scope.removeTelefone = function(){
+          delete $scope.contato;
+          $scope.clienteContatoForm.$setPristine();
+        };
+        $scope.AddTelefone = function(telefone){
+          $scope.telefones.push(telefone);
+          console.log($scope.telefones);
+          delete $scope.contato.telefone;
+        };
       }
     }
   }
